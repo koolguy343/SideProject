8daae712ea04bbf14072d14f09ef0d686cec69c9
Brandon Touchet <brandontouchet@me.com>
2013-09-24 15:55:23 -0500

    Start Stock code

---
+++ Stocks/Stocks/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Stocks
+//
+//  Created by Brandon Touchet on 9/24/13.
+//  Copyright (c) 2013 Brandon Touchet. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
